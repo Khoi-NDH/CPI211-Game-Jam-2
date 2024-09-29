355401ad45a12421d9ddd4c7e91adb6e9aa18a46
Khoi-NDH <107507727+Khoi-NDH@users.noreply.github.com>
2024-09-29 04:37:57 -0700

    few changes on collide Listener

--- Assets/Scripts/Sound/CollideListener.cs
+++ Assets/Scripts/Sound/CollideListener.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class CollideListener : MonoBehaviour
 {
     // Start is called before the first frame update
-    private GameObject GenericObject;
+    public GameObject GenericObject1;
+    public GameObject GenericObject2;
+    public GameObject GenericObject3;
+    //public GameObject GenericObject4;
+    //public GameObject GenericObject5;
     public GameObject floor;
     public GameObject GenericSoundPrefab;
     private bool collided = false;
@@ -15,7 +19,7 @@ public class CollideListener : MonoBehaviour
     void Start()
     {
 
-        GenericObject = GameObject.FindGameObjectWithTag("AbleToGrab");
+        
         collided = false;
 
     }
@@ -31,13 +35,21 @@ public class CollideListener : MonoBehaviour
     void FixedUpdate()
     {
         collided = floor.GetComponent<PropSound>().collided;
-   
+
 
         if (collided == true && collision_occured == false)
         {
-            GameObject instance = Instantiate(GenericSoundPrefab, GenericObject.transform.position, Quaternion.identity);
-    
+            GameObject instance = Instantiate(GenericSoundPrefab, GenericObject1.transform.position, Quaternion.identity);
+            GameObject instance2 = Instantiate(GenericSoundPrefab, GenericObject2.transform.position, Quaternion.identity);
+            GameObject instance3 = Instantiate(GenericSoundPrefab, GenericObject3.transform.position, Quaternion.identity);
+            //GameObject instance4 = Instantiate(GenericSoundPrefab, GenericObject4.transform.position, Quaternion.identity);
+            //GameObject instance5 = Instantiate(GenericSoundPrefab, GenericObject5.transform.position, Quaternion.identity);
+
             Destroy(instance,1f);
+            Destroy(instance2, 1f);
+            Destroy(instance3, 1f);
+            //Destroy(instance4, 1f);
+            //Destroy(instance5, 1f);
             collision_occured = true;
             Debug.Log("Impact!!!!");
 
