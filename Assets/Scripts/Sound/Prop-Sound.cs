ec4b937cc79e3c5379301bf5624e4bb48ca27271
Austin-Mayhew <atmayhew@asu.edu>
2024-09-30 13:44:44 -0700

    Adjusted a few throwables

--- Assets/Scripts/Sound/Prop-Sound.cs
+++ Assets/Scripts/Sound/Prop-Sound.cs
@@ -8,35 +8,25 @@ public class PropSound : MonoBehaviour
     public GameObject GenericSoundPrefab;
     private GameObject GenericObject;
     public bool collided = false;
+    private bool waitingToStart = true;
     
-    void Start() {
-
+    void Start()
+    {
         GenericObject = gameObject;
-
-
+        Invoke("EnableSound", 0.5f);
     }
+
     private void OnCollisionEnter(Collision Collision)
     {
-        
-       
-
+        if (!waitingToStart)
+        {
+            GameObject instance = Instantiate(GenericSoundPrefab, GenericObject.transform.position, Quaternion.identity);
+            Destroy(instance, 0.5f);
+        }
     }
 
-    private void OnCollisionExit(Collision Collision)
+    private void EnableSound()
     {
-        GameObject instance = Instantiate(GenericSoundPrefab, GenericObject.transform.position, Quaternion.identity);
-        Destroy(instance, 0.5f);
-    }
-
-
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-       
-
+        waitingToStart = false;
     }
 }
