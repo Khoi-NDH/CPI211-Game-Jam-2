621b0c3cb43f6b72298becc9d274d3ee77abffec
jasLubovac <jas.lubovo@gmail.com>
2024-09-30 23:34:54 -0700

    functional main menu buttons
    buttons now work, added cricket sounds to main menu

--- Assets/MainMenu/MainMenu_script.cs
+++ Assets/MainMenu/MainMenu_script.cs
@@ -1,18 +1,25 @@
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu_script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    // start button
+    public void start() {
+
+        // from scene 0 to scene 1 (in build settings)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    // quit button
+    public void quit() {
+
+        Application.Quit();
+        Debug.Log("quit game");
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
